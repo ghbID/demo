48304714bd6b6440f3417e72911c1b3d1a8eb3b0
myname <myemail@qq.com>
2022-10-04 23:47:41 +0800

    second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,7 +21,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-//        System.out.println("hello");
+        System.out.println("hello");
 
 
     }
